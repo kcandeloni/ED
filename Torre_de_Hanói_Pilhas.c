478a20b63c2de4375ea0d6bce949410cf1d516c2
kcandeloni <kcandeloni@gmail.com>
2017-04-18 23:28:21 -0300

    Torre de Hanói utilizando pilhas
    Implementação do um algoritmo em linguagem C, ue resolve a Torre de
    Hanói utilizando estruturas de pilhas.

--- Torre_de_Hanói_Pilhas.c
+++ Torre_de_Hanói_Pilhas.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-
 
 struct lista {
     int tamDisco;
@@ -15,7 +13,7 @@ struct pilha {
 
 typedef struct pilha Pilha;
 
-Pilha *pilhaA= NULL;
+Pilha *pilhaA= NULL; /* Variaeis globais de cada torre*/
 Pilha *pilhaB= NULL;
 Pilha *pilhaC= NULL;
 
@@ -54,11 +52,14 @@ int pop (Pilha *p) {
     	i = temp-> tamDisco;
     	p -> topo = temp-> prox;
     	free(temp);
-    	return i;
+    	return i; /* Retorna o valor do disco que esta sendo desalocado */
 	}
-	else return 0;
+	else return 0; /* retorna 0 caso a pilha esteja vazia */
 }
 
+/* Função recursiva que resolve a Torre de Hanoi, com o meor numero de
+movimentos.
+O numero de movimentos que seraão necessarios pode ser demonstrado pela formula: (2^nDiscos ) - 1 */
 void moveDiscos(Pilha *torre1, Pilha *torre3, Pilha *torre2, int nDiscos){
 
 	static int move=1;
@@ -66,13 +67,14 @@ void moveDiscos(Pilha *torre1, Pilha *torre3, Pilha *torre2, int nDiscos){
 	if(nDiscos > 0){
 		
 		moveDiscos(torre1, torre2, torre3, nDiscos-1);
-		push(torre3, pop(torre1));
-		printf("\n\n\t------- Movimento %d -------", move++);
-		imprimePilha(torre1, torre2, torre3);
+		push(torre3, pop(torre1)); /* Realiza o movimento do disco */
+		printf("\n\n\t------- Movimento %d -------", move++); /* imprime o numero do movimento */
+		imprimePilha(); /* Imprime o estado atual das torres apos cada movimento realizado */
 		moveDiscos(torre2, torre3, torre1, nDiscos-1);
 		}		
 }
 
+/* Imprime a pilhas */
 void imprimePilha(){
 	
 	Lista *percorre1;
@@ -83,12 +85,13 @@ void imprimePilha(){
 	percorre3 = pilhaC->topo;
 	
 	printf("\n\n\tTorre A\tTorre B\tTorre C");
-	/*Enquanto alguma das pilhas ainda não chegou ao final*/
+	/* Enquanto alguma das pilhas ainda nao chegou ao final, o laço continua sendo executado */
 	while(percorre1 != NULL || percorre2 != NULL || percorre3 != NULL){
 		printf("\n\t");
-		if(percorre1!=NULL){
-			printf("%d", percorre1->tamDisco);
-			percorre1 = percorre1->prox;
+		if(percorre1!=NULL){ /* Se a torre 1 já chegou ao final, ela não sera mais impressa */
+			printf("%d", percorre1->tamDisco); /* Imprime os discos presentes na torre 1 */
+			percorre1 = percorre1->prox; /* O ponteiro da torre 1 recebe o valor do proximo 
+			elemento da pilha */
 		}	
 		printf("\t");
 		if(percorre2!=NULL){
@@ -101,7 +104,6 @@ void imprimePilha(){
 			percorre3 = percorre3->prox;
 		}
 	}
-
 }
 
 /* funcao principal */
@@ -114,25 +116,23 @@ int main(void) {
     int nDiscos, i;
     
 	printf("Numero de Discos: ");
-	scanf("%d", &nDiscos);
+	scanf("%d", &nDiscos); /* Define o numeo de discos */
 	
 	for(i=nDiscos; i>0; i--){
 		printf("push %d\n", i);
-    	push(pilhaA, i);
+    	push(pilhaA, i); /* Insere os discos na pilha */
     }
     
     printf("\n\n\t------ Estado Inicial ------");
-    imprimePilha();
-	moveDiscos(pilhaA, pilhaC, pilhaB, nDiscos);
+    imprimePilha(); /* Imprime a torre de hanoi antes de começar a moimentar os discos */
+	moveDiscos(pilhaA, pilhaC, pilhaB, nDiscos); /* Chama a função que realizara os movimentos dos discos */
     
     printf("\n\nFree\tTorre A\tTorre B\tTorre C");
     for(i=nDiscos; i>0; i--){
-		printf("\n\tpop %d\tpop %d\tpop %d", pop(pilhaA), pop(pilhaB), pop(pilhaC));
+		printf("\n\tpop %d\tpop %d\tpop %d", pop(pilhaA), pop(pilhaB), pop(pilhaC)); /* Desaloca as pilhas */
     }
     
     // system("pause");
     return 0;
 }
-
-
 
